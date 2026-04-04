9dcb32281231627848e325029a72598121543c59
Ruhul <itsruhul@proton.me>
2026-04-04 13:40:07 +0530

    work on medicine page

---
+++ src/scripts/medicinesData.js
@@ -0,0 +1,18 @@
+import pillIcon from '../assets/pill.svg';
+
+export const medicinesData = [
+  {
+    id: 1,
+    name: 'Aspirin',
+    time: '08:00 AM', // Example format, DayJS will parse this
+    icon: pillIcon,
+    instruction: 'Have after meal, 1 tablet with water'
+  },
+  {
+    id: 2,
+    name: 'Vitamin C',
+    time: '02:30 PM',
+    icon: pillIcon,
+    instruction: 'Afternoon supplement, chewable'
+  }
+];
